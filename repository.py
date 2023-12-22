eabcdb9afd50c1544b89f07cc0f2c5c67e141bc5
msolorio <msolorio.github@gmail.com>
2023-12-21 18:39:23 -0800

    Tests repository

--- repository.py
+++ repository.py
@@ -1,3 +1,4 @@
+from typing import List
 import abc
 import model.model as model
 
@@ -19,3 +20,20 @@ class SqlAlchemyRepository(AbstractRepository):
     
     def get(self, reference: model.Reference) -> model.Batch:
         return self.session.query(model.Batch).filter_by(reference=reference).first()
+    
+    def list(self) -> List[model.Batch]:
+        return self.session.query(model.Batch).all()
+
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches: List[model.Batch]):
+        self._batches = set(batches)
+    
+    def add(self, batch: model.Batch):
+        self._batches.add(batch)
+    
+    def get(self, reference: model.Reference) -> model.Batch:
+        return next(b for b in self._batches if b.reference == reference)
+    
+    def list(self) -> List[model.Batch]:
+        return list(self._batches)
