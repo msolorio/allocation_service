25f3e26447fbe7362a1f39ba75258884ac6b0055
msolorio <msolorio.github@gmail.com>
2024-01-09 11:03:42 -0800

    Remove model dependencies from service layer tests

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,7 +1,6 @@
 import pytest
 import service_layer.services as services
 from adapters.repository import FakeRepository
-import domain.model as model
 from tests.helpers import random_sku, random_batchref
 
 
@@ -13,9 +12,9 @@ class FakeSession:
 
 
 def test_commits():
-    batch = model.Batch("batch1", "LAMP", 100, "2000-01-01")
-    repo = FakeRepository([batch])
+    repo = FakeRepository([])
     session = FakeSession()
+    services.add_batch("batch1", "LAMP", 100, "2000-01-01", repo, session)
     services.allocate("o1", "LAMP", 10, repo, session)
 
     assert session.commited
@@ -29,10 +28,9 @@ def test_add_batch():
 
 
 def test_returns_allocation():
-    batch = model.Batch("batch1", "LAMP", 100, "2000-01-01")
-    repo = FakeRepository([batch])
-
-    result = services.allocate("o1", "LAMP", 10, repo, FakeSession())
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("batch1", "LAMP", 100, "2000-01-01", repo, session)
+    result = services.allocate("o1", "LAMP", 10, repo, session)
     assert result == "batch1"
 
 
@@ -40,31 +38,21 @@ def test_allocate_saves_allocations():
     sku = random_sku()
     earliest_batchref = random_batchref("earliest")
     later_batchref = random_batchref("later")
-    repo = FakeRepository(
-        [
-            model.Batch(earliest_batchref, sku, 10, "2000-01-01"),
-            model.Batch(later_batchref, sku, 10, "2000-01-02"),
-        ]
-    )
 
-    result1 = services.allocate("o1", sku, 10, repo, FakeSession())
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch(earliest_batchref, sku, 10, "2000-01-01", repo, session)
+    services.add_batch(later_batchref, sku, 10, "2000-01-02", repo, session)
+
+    result1 = services.allocate("o1", sku, 10, repo, session)
     assert result1 == earliest_batchref
 
-    result2 = services.allocate("o2", sku, 10, repo, FakeSession())
+    result2 = services.allocate("o2", sku, 10, repo, session)
     assert result2 == later_batchref
 
 
 def test_invalid_sku():
-    batch = model.Batch("batch1", "COUCH", 100, "2000-01-01")
-    repo = FakeRepository([batch])
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("batch1", "COUCH", 100, "2000-01-01", repo, session)
 
     with pytest.raises(services.InvalidSku):
-        services.allocate("o1", "LAMP", 10, repo, FakeSession())
-
-
-def test_out_of_stock():
-    batch = model.Batch("batch1", "LAMP", 10, "2000-01-01")
-    repo = FakeRepository([batch])
-
-    with pytest.raises(model.OutOfStock):
-        services.allocate("o1", "LAMP", 20, repo, FakeSession())
+        services.allocate("o1", "LAMP", 10, repo, session)
