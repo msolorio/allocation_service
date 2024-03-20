81e3c8e53d2d1a6a844b4c7f90b4daec631738bc
msolorio <msolorio.github@gmail.com>
2024-03-20 15:17:24 -0700

    Domain model - cannot allocate line more than once to a batch

--- src/app/domain/model.py
+++ src/app/domain/model.py
@@ -2,11 +2,23 @@ class Batch:
     def __init__(self, batch_ref, sku, qty):
         self.batch_ref = batch_ref
         self.sku = sku
-        self.available_quantity = qty
+        self.initial_quantity = qty
+        self._allocations = set()
 
     def allocate(self, line):
-        if self.available_quantity >= line.qty:
-            self.available_quantity -= line.qty
+        if self.can_allocate(line) and line not in self._allocations:
+            self._allocations.add(line)
+
+    def can_allocate(self, line):
+        return self.available_quantity >= line.qty
+
+    @property
+    def allocated_quantity(self):
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self):
+        return self.initial_quantity - self.allocated_quantity
 
 
 class OrderLine:
--- tests/unit/test_batch.py
+++ tests/unit/test_batch.py
@@ -21,10 +21,20 @@ def test_cannot_allocate_to_batch_if_not_enough_available_quantity():
     assert batch.available_quantity == 5
 
 
-def test_can_allocate_avaiable_quantity_is_equal_to_line_quantity():
+def test_can_allocate_if_avaiable_quantity_is_equal_to_line_quantity():
     batch = Batch(batch_ref="batch-001", sku="SMALL-TABLE", qty=5)
     line = OrderLine(orderid="order-123", sku="SMALL-TABLE", qty=5)
 
     batch.allocate(line)
 
     assert batch.available_quantity == 0
+
+
+def test_cannot_allocate_line_to_batch_more_than_once():
+    batch = Batch(batch_ref="batch-001", sku="SMALL-TABLE", qty=20)
+    line = OrderLine(orderid="order-123", sku="SMALL-TABLE", qty=2)
+
+    batch.allocate(line)
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
