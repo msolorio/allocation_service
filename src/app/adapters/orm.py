54c1a49e071148907628792d2760578d4a41b83a
msolorio <msolorio.github@gmail.com>
2024-03-22 09:28:30 -0700

    Adds batch data mapper

--- src/app/adapters/orm.py
+++ src/app/adapters/orm.py
@@ -1,7 +1,7 @@
 from sqlalchemy import MetaData, Table, Column, String, Integer
 from sqlalchemy.orm import mapper
 
-from app.domain.model import OrderLine
+from app.domain.model import OrderLine, Batch
 
 metadata = MetaData()
 
@@ -9,11 +9,22 @@ order_lines = Table(
     "order_lines",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("orderid", String(255)),
-    Column("sku", String(255)),
+    Column("orderid", String(255), nullable=False),
+    Column("sku", String(255), nullable=False),
     Column("qty", Integer, nullable=False),
+)
+
+batches = Table(
+    "batches",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("batch_ref", String(255), nullable=False),
+    Column("sku", String(255), nullable=False),
+    Column("initial_quantity", Integer, nullable=False),
+    Column("eta", String(255)),
 )
 
 
 def start_mappers():
     mapper(OrderLine, order_lines)
+    mapper(Batch, batches)
--- tests/integration/test_orm.py
+++ tests/integration/test_orm.py
@@ -1,6 +1,7 @@
 import pytest
+from datetime import date
 
-from app.domain.model import OrderLine
+from app.domain.model import OrderLine, Batch
 
 
 def test_orderline_mapper_can_load_lines(session):
@@ -10,7 +11,6 @@ def test_orderline_mapper_can_load_lines(session):
         "('order1', 'BLUE-CHAIR', 24),"
         "('order2', 'RED-TABLE', 1)"
     )
-
     expected = [
         OrderLine(orderid="order1", sku="RED-CHAIR", qty=12),
         OrderLine(orderid="order1", sku="BLUE-CHAIR", qty=24),
@@ -18,3 +18,39 @@ def test_orderline_mapper_can_load_lines(session):
     ]
 
     assert session.query(OrderLine).all() == expected
+
+
+def test_orderline_mapper_can_save_lines(session):
+    new_line = OrderLine(orderid="order1", sku="ALARM-CLOCK", qty=10)
+    session.add(new_line)
+    session.commit()
+
+    rows = list(session.execute("SELECT orderid, sku, qty FROM order_lines"))
+    assert rows == [("order1", "ALARM-CLOCK", 10)]
+
+
+def test_batch_mapper_can_load_batches(session):
+    session.execute(
+        "INSERT INTO batches (batch_ref, sku, initial_quantity, eta) VALUES "
+        "('batch1', 'GENERIC-SOFA', 20, null),"
+        "('batch2', 'LAMP', 100, '2020-01-01'),"
+        "('batch3', 'CHAIR', 50, '2020-01-02')"
+    )
+    expected = [
+        Batch(batch_ref="batch1", sku="GENERIC-SOFA", qty=20, eta=None),
+        Batch(batch_ref="batch2", sku="LAMP", qty=100, eta=date(2020, 1, 1)),
+        Batch(batch_ref="batch3", sku="CHAIR", qty=50, eta=date(2020, 1, 2)),
+    ]
+
+    assert session.query(Batch).all() == expected
+
+
+def test_batch_mapper_save_batches(session):
+    new_batch = Batch(batch_ref="batch1", sku="GENERIC-SOFA", qty=20)
+    session.add(new_batch)
+    session.commit()
+
+    rows = list(
+        session.execute("SELECT batch_ref, sku, initial_quantity, eta FROM batches")
+    )
+    assert rows == [("batch1", "GENERIC-SOFA", 20, None)]
